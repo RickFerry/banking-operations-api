9b1e14c4476f9d713624524c255f3bc2467102f0
Ricardo <rick_ferry1@hotmail.com>
2024-08-02 16:39:59 -0300

    Ajuste.

--- src/test/java/com/example/banking/BankingOperationsApiApplicationTests.java
+++ src/test/java/com/example/banking/BankingOperationsApiApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.banking;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class BankingOperationsApiApplicationTests {
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        Assertions.assertThat(applicationContext).isNotNull();
     }
-
 }
